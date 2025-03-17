72a39e62569e84be793c85441dd491ac699f26b0
Renz Gabriel E. Salas <124060701+RenzGabrielSalas@users.noreply.github.com>
2025-03-18 02:50:56 +0800

    Add color variables for profile page

--- lib/appcolors.dart
+++ lib/appcolors.dart
@@ -4,4 +4,7 @@ class AppColors {
   static const Color bgColor = Color(0xFF263A56);
   static const Color mainColor = Color(0xFFE09500);
   static const Color accentColor = Color(0xFF080C1E);
+
+  static const Color pageColor = Color(0xFFE2E2E2);
+  static const Color cardColor = Color(0xFFF2F2F2);
 }
