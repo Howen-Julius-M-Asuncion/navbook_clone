9bb3934477beb0b935a5548ffda3ce0fcbc49244
Renz Gabriel E. Salas <124060701+RenzGabrielSalas@users.noreply.github.com>
2025-03-18 02:51:33 +0800

    Refactor homepage to use PageScaffold instead of TabScaffold

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
-import 'appcolors.dart';
 import 'main.dart';
-
+import 'appcolors.dart';
+import 'profilepage.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -16,239 +16,183 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        activeColor: AppColors.mainColor,
-        inactiveColor: AppColors.accentColor,
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-          switch (index) {
-            case 0:
-              setState(() {
-                _selectedPage = 'Home';
-              });
-              break;
-            case 1:
-              setState(() {
-                _selectedPage = 'Navigate';
-              });
-              break;
-            case 2:
-              setState(() {
-                _selectedPage = 'Schedule';
-              });
-              break;
-            case 3:
-              setState(() {
-                _selectedPage = 'Notifications';
-              });
-              break;
-          }
-        },
-        height: 75,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(_selectedIndex == 0 ? CupertinoIcons.house_fill : CupertinoIcons.house),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(_selectedIndex == 1 ? CupertinoIcons.map_pin_ellipse: CupertinoIcons.map_pin),
-            label: 'Navigate',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(_selectedIndex == 2 ? CupertinoIcons.calendar_today: CupertinoIcons.calendar),
-            label: 'Schedule',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(_selectedIndex == 3 ? CupertinoIcons.bell_fill : CupertinoIcons.bell),
-            label: 'Notifications',
-          ),
-        ],
-      ),
-      tabBuilder: (context, index) {
-        return CupertinoTabView(
-          builder: (context) {
-            return CupertinoPageScaffold(
-              navigationBar: CupertinoNavigationBar(
-                automaticBackgroundVisibility: false,
-                automaticallyImplyLeading: false,
-                leading: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    CupertinoButton(
-                      padding: EdgeInsets.zero, // Remove default padding
-                      onPressed: () {},
-                      child: SizedBox(
-                        height: 30,
-                        width: 30,
-                        child: ClipOval(
-                          child: Image.asset(
-                            'images/default_profile.png',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 6),
-                    Text(
-                      '$_selectedPage',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        automaticBackgroundVisibility: false,
+        automaticallyImplyLeading: false,
+        leading: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CupertinoButton(
+              padding: EdgeInsets.zero, // Remove default padding
+              onPressed: () {
+                Navigator.of(
+                    context,
+                    rootNavigator: true).push(
+                  CupertinoPageRoute(builder: (context) => Profilepage()),
+                );
+              },
+              child: SizedBox(
+                height: 30,
+                width: 30,
+                child: ClipOval(
+                  child: Image.asset(
+                    'images/default_profile.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    CupertinoButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () {
-                        showCupertinoDialog(
-                          context: context,
-                          builder: (context) {
-                            return CupertinoAlertDialog(
-                              title: Text('Developers'),
-                              content: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.all(16.0),
-                                        child: Column(
-                                          children: [
-                                            ClipOval(
-                                              child: Image.asset(
-                                                'images/devs/howen.jpg',
-                                                height: 75,
-                                                width: 75,
-                                                fit: BoxFit.cover,
-                                              ),
-                                            ),
-                                            Text('Howen Julius Asuncion'),
-                                          ],
-                                        ),
+              ),
+            ),
+            SizedBox(width: 6),
+            Text(
+              '$_selectedPage',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CupertinoButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) {
+                    return CupertinoAlertDialog(
+                      title: Text('Developers'),
+                      content: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Column(
+                                  children: [
+                                    ClipOval(
+                                      child: Image.asset(
+                                        'images/devs/howen.jpg',
+                                        height: 75,
+                                        width: 75,
+                                        fit: BoxFit.cover,
                                       ),
-                                    ],
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.all(16.0),
-                                        child: Column(
-                                          children: [
-                                            ClipOval(
-                                              child: Image.asset(
-                                                'images/devs/goco.jpg',
-                                                height: 75,
-                                                width: 75,
-                                                fit: BoxFit.cover,
-                                              ),
-                                            ),
-                                            Text('John Michael Goco'),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.all(16.0),
-                                        child: Column(
-                                          children: [
-                                            ClipOval(
-                                              child: Image.asset(
-                                                'images/devs/renz.jpg',
-                                                height: 75,
-                                                width: 75,
-                                                fit: BoxFit.cover,
-                                              ),
-                                            ),
-                                            Text('Renz Gabriel Salas'),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              actions: [
-                                CupertinoButton(
-                                  child: Text('Close', style: TextStyle(color: CupertinoColors.destructiveRed)),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
+                                    ),
+                                    Text('Howen Julius Asuncion'),
+                                  ],
                                 ),
-                              ],
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Column(
+                                  children: [
+                                    ClipOval(
+                                      child: Image.asset(
+                                        'images/devs/goco.jpg',
+                                        height: 75,
+                                        width: 75,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Text('John Michael Goco'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Column(
+                                  children: [
+                                    ClipOval(
+                                      child: Image.asset(
+                                        'images/devs/renz.jpg',
+                                        height: 75,
+                                        width: 75,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Text('Renz Gabriel Salas'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      actions: [
+                        CupertinoButton(
+                          child: Text('Close', style: TextStyle(color: CupertinoColors.destructiveRed)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Icon(
+                CupertinoIcons.info_circle,
+                color: CupertinoColors.darkBackgroundGray,
+                size: 25,
+              ),
+            ),
+            // ------------- LOGOUT ICON -------------
+            CupertinoButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) {
+                    return CupertinoAlertDialog(
+                      title: Text('Logout'),
+                      content: Text('Are you sure?'),
+                      actions: [
+                        CupertinoButton(
+                          child: Text("Yes", style: TextStyle(color: CupertinoColors.destructiveRed)),
+                          onPressed: () {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              CupertinoPageRoute(builder: (context) => MyApp()),
+                              (route) => false,
                             );
                           },
-                        );
-                      },
-                      child: Icon(
-                        CupertinoIcons.info_circle,
-                        color: CupertinoColors.darkBackgroundGray,
-                        size: 25,
-                      ),
-                    ),
-                    // ------------- LOGOUT ICON -------------
-                    // CupertinoButton(
-                    //   padding: EdgeInsets.zero,
-                    //   onPressed: () {
-                    //     showCupertinoDialog(
-                    //       context: context,
-                    //       builder: (context) {
-                    //         return CupertinoAlertDialog(
-                    //           title: Text('Logout'),
-                    //           content: Text('Are you sure?'),
-                    //           actions: [
-                    //             CupertinoButton(
-                    //               child: Text("Yes", style: TextStyle(color: CupertinoColors.destructiveRed)),
-                    //               onPressed: () {
-                    //                 Navigator.pushReplacement(
-                    //                   context,
-                    //                   CupertinoPageRoute(builder: (context) => MyApp()),
-                    //                 );
-                    //               },
-                    //             ),
-                    //             CupertinoButton(
-                    //               child: Text("No"),
-                    //               onPressed: () {
-                    //                 Navigator.pop(context);
-                    //               },
-                    //             ),
-                    //           ],
-                    //         );
-                    //       },
-                    //     );
-                    //   },
-                    //   child: Icon(
-                    //     CupertinoIcons.arrow_uturn_left_circle,
-                    //     color: CupertinoColors.darkBackgroundGray,
-                    //     size: 25,
-                    //   ),
-                    // ),
-                  ],
-                ),
+                        ),
+                        CupertinoButton(
+                          child: Text("No"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Icon(
+                CupertinoIcons.arrow_uturn_left_circle,
+                color: CupertinoColors.darkBackgroundGray,
+                size: 25,
               ),
-              child: SafeArea(
-                child: Column(
-                  children: [
+            ),
+          ],
+        ),
+      ),
+      child: SafeArea(child: Column(
 
-                  ],
-                )
-              ),
-            );
-          },
-        );
-      },
+      )),
     );
   }
 }
