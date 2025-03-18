b62c00a898dab920cf880ecd88009b5d4c57742a
John Michael Goco <116796110+John-Michael-Goco@users.noreply.github.com>
2025-03-18 20:26:12 +0800

    "Fill with placeholder cards and add search"

--- lib/tab/navigate.dart
+++ lib/tab/navigate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import '/page/profile.dart';
 import '/main.dart';
 import '/appcolors.dart';
@@ -16,6 +17,7 @@ class _NavpageState extends State<Navpage> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.lightBackgroundGray,
       navigationBar: CupertinoNavigationBar(
         automaticBackgroundVisibility: false,
         automaticallyImplyLeading: false,
@@ -32,8 +34,8 @@ class _NavpageState extends State<Navpage> {
                 );
               },
               child: SizedBox(
-                height: 30,
-                width: 30,
+                height: 35,
+                width: 35,
                 child: ClipOval(
                   child: Image.asset(
                     'images/default_profile.png',
@@ -52,16 +54,155 @@ class _NavpageState extends State<Navpage> {
             ),
           ],
         ),
+        trailing: CupertinoButton(child: Icon(CupertinoIcons.search), onPressed: (){}),
       ),
-      child: SafeArea(child:
-      Column(
+      child: SafeArea(child: Column(
         children: [
-          Text('Navigate')
-
-
+          Expanded(
+            child: ListView(
+              // ContentCards
+              children: [
+                // With attachment
+                Card.filled(
+                  margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Row for Image
+                        Container(
+                          width: double.infinity,
+                          height: 200,
+                          child: FittedBox(
+                            fit: BoxFit.cover, // Ensures the image fills the width, even if it needs to zoom in
+                            clipBehavior: Clip.hardEdge, // Prevents overflow issues
+                            child: Image.asset(
+                              'images/rooms/building_default.jpg',
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        // Row for Description
+                        Text(
+                          'Room Computer Lab A',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                          softWrap: true,
+                        ),
+                        Row(children: [
+                          Flexible(
+                            child: Text(
+                              'Located in SHS Building 3rd Floor',
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 16,
+                              ),
+                              softWrap: true,
+                            ),
+                          ),
+                        ],)
+                      ]
+                    ),
+                  ),
+                ),
+                Card.filled(
+                  margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Row for Image
+                          Container(
+                            width: double.infinity,
+                            height: 200,
+                            child: FittedBox(
+                              fit: BoxFit.cover, // Ensures the image fills the width, even if it needs to zoom in
+                              clipBehavior: Clip.hardEdge, // Prevents overflow issues
+                              child: Image.asset(
+                                'images/rooms/building_default.jpg',
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          // Row for Description
+                          Text(
+                            'Room Computer Lab B',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                            softWrap: true,
+                          ),
+                          Row(children: [
+                            Flexible(
+                              child: Text(
+                                'Located in SHS Building 3rd Floor',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 16,
+                                ),
+                                softWrap: true,
+                              ),
+                            ),
+                          ],)
+                        ]
+                    ),
+                  ),
+                ),
+                Card.filled(
+                  margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Row for Image
+                          Container(
+                            width: double.infinity,
+                            height: 200,
+                            child: FittedBox(
+                              fit: BoxFit.cover, // Ensures the image fills the width, even if it needs to zoom in
+                              clipBehavior: Clip.hardEdge, // Prevents overflow issues
+                              child: Image.asset(
+                                'images/rooms/building_default.jpg',
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          // Row for Description
+                          Text(
+                            'Room CE Lab',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                            softWrap: true,
+                          ),
+                          Row(children: [
+                            Flexible(
+                              child: Text(
+                                'Located in SHS Building 1st Floor',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 16,
+                                ),
+                                softWrap: true,
+                              ),
+                            ),
+                          ],)
+                        ]
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ),
         ],
-      )
-      ),
+      )),
     );
   }
 }
